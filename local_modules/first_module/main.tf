684b3e74f5cc2392bba2b7b7249e730b91b7d3af
Mark Ryan <markryan@nuna.com>
2021-01-30 13:23:41 -0500

    this still applies cleanly

--- local_modules/first_module/main.tf
+++ local_modules/first_module/main.tf
@@ -6,10 +6,10 @@ provider "aws" {
   alias = "first_east"
 }
 
-data "aws_caller_identity" "first_west" {
-  provider = aws.first_west
-}
-
-data "aws_caller_identity" "first_east" {
-  provider = aws.first_east
-}
+#module "second" {
+#  providers = {
+#    aws.second_west = aws.first_west
+#    aws.second_east = aws.first_east
+#  }
+#  source = "../second_module"
+#}
